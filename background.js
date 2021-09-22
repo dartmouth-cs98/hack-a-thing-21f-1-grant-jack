06dbc1da7f967ee7405bec61020841f74df2d17b
Grant Dumanian <grant.dumanian@mana.ai>
2021-09-21 22:13:22 -0400

    new version of content js

--- background.js
+++ background.js
@@ -3,18 +3,14 @@ console.log("background running");
 chrome.browserAction.onClicked.addListener(setup);
 function setup() {
     noCanvas();
-    userinputFrom = select('#change_from');
-    userinputTo = select('#change_to');
-    
-    userinputFrom.input(sendText);
+    userinput = select('#change_from');
+    userinput.input(sendText);
 
 
 
     function sendText() {
         //Value got from input field in popup
-        message = userinputFrom.value();
-        otherMessage = userinputTo.value();
-        console.log(otherMessage)
+        message = userinput.value();
         //Sending message to content
         chrome.tabs.query({active: true,currentWindow:true},function(tabs){
             chrome.tabs.sendMessage(tabs[0].id,message);
--- scripts/content.js
+++ scripts/content.js
@@ -15,7 +15,6 @@ console.log("Chrome Extension ready to go!");
 chrome.runtime.onMessage.addListener(replace);
 //Replace
 function replace(message, sender, sendresponse) {
-  console.log(message);
   paragraphs = document.getElementsByTagName("p");
   for (elt of paragraphs) {
     elt.innerText = message;
