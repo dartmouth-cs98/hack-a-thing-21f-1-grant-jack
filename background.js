a1b6a630d1f769ad0e60934a578621725154663c
Grant Dumanian <grant.dumanian@mana.ai>
2021-09-21 21:30:55 -0400

    text swap works

---
+++ background.js
@@ -0,0 +1,23 @@
+console.log("background running");
+
+chrome.browserAction.onClicked.addListener(setup);
+function setup() {
+    noCanvas();
+    userinputFrom = select('#change_from');
+    userinputTo = select('#change_to');
+    
+    userinputFrom.input(sendText);
+
+
+
+    function sendText() {
+        //Value got from input field in popup
+        message = userinputFrom.value();
+        otherMessage = userinputTo.value();
+        console.log(otherMessage)
+        //Sending message to content
+        chrome.tabs.query({active: true,currentWindow:true},function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id,message);
+        });
+    }
+}
