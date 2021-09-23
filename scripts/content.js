6339bd32df406df6628883ca18a22fc962b15439
Grant Dumanian <grant.dumanian@mana.ai>
2021-09-23 13:21:11 -0400

    button listener implemented, close to working

--- scripts/content.js
+++ scripts/content.js
@@ -10,41 +10,57 @@
 //     text[i].innerHTML = text[i].innerHTML.replace('Times', 'Nick Schoeller');
 //   }
 // }
+// document.getElementById("subBut").addEventListener('click', function() {
+//   console.log("clicked");
+//   swap(fromMessage, toMessage);
+// });
 
-function swap(from, to) {
-  text = document.getElementsByTagName('*');
+fromMessage = ""
+toMessage = ""
 
-  for (i = 0; i < text.length; i++) {
-    if(from.length > 3 && from != "" && to != ""){
-      if (text[i].innerHTML.includes(from)) {
-        text[i].innerHTML = text[i].innerHTML.replace(from, to);
-      }
-    }
-  }
-  console.log("swapped");
-}
 
-var fromMessage = ""
-var toMessage = ""
+// document.getElementById("subBut").onClicked = swap(fromMessage, toMessage);
 
-document.getElementById("subBut").onclick = swap(fromMessage, toMessage);
 console.log("Chrome Extension ready to go!");
 
 chrome.runtime.onMessage.addListener(replace);
 //Replace
 function replace(message, sender, sendresponse) {
   if(message.toMessage != null){
-    console.log("toMessage");
     toMessage = message.toMessage;
+    console.log(toMessage);
   } 
   if(message.fromMessage != null){
-    console.log("fromMessage");
     fromMessage = message.fromMessage;
+    console.log(fromMessage);
   }
-  // paragraphs = document.getElementsByTagName("p");
-  // for (elt of paragraphs) {
-  //   elt.innerText = fromMessage.concat(toMessage);
-  // }
+
   text = document.getElementsByTagName('*');
 
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var subBut = document.getElementById('subBut');
+  subBut.addEventListener('click', function() {
+    text = document.getElementsByTagName('*');
+    alert(fromMessage);
+    for (i = 0; i < text.length; i++) {
+      if (text[i].innerHTML.includes(fromMessage)) {
+        text[i].innerHTML = text[i].innerHTML.replace(fromMessage, toMessage);
+      }
+    }
+    });
+});
+
+// function swap(from, to) {
+//   text = document.getElementsByTagName('*');
+
+//   for (i = 0; i < text.length; i++) {
+//     if(from.length > 3 && from != "" && to != ""){
+//       if (text[i].innerHTML.includes(from)) {
+//         text[i].innerHTML = text[i].innerHTML.replace(from, to);
+//       }
+//     }
+//   }
+// }
